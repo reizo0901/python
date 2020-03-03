e21a5f773a634d60f7926e89c3b9140af2085d02
reizo0901 <h.lemon.hart0901@gmail.com>
2020-03-03 18:49:39 +0900

    add new file

---
+++ test02.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python
+# _*_ coding:utf8 _*_
+print('モジュールのロード')
+def test():
+	print('関数:testを呼び出しました')
+
+if __name__ == '__main__':
+	print('python-izm')
+#	print('パイソンイズム')
+	test()
